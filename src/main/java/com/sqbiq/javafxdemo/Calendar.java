785d8ce42e23e397bfd78f687f1f594c6153c80d
Sebastian <sebasqbi@gmail.com>
2024-02-26 17:15:52 +0100

    Change class to VBox

--- src/main/java/com/sqbiq/javafxdemo/Calendar.java
+++ src/main/java/com/sqbiq/javafxdemo/Calendar.java
@@ -1,10 +1,10 @@
 package com.sqbiq.javafxdemo;
 
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.time.Month;
 
-public class Calendar extends HBox {
+public class Calendar extends VBox {
     private CalendarGrid calendarGrid = new CalendarGrid();
 
     public Calendar() {
