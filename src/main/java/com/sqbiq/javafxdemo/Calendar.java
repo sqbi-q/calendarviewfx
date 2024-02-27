f48fb3570d713b7d1c25124e7d305ff0a2574958
Sebastian <sebasqbi@gmail.com>
2024-02-27 09:17:13 +0100

    Display date on calendar grid

--- src/main/java/com/sqbiq/javafxdemo/Calendar.java
+++ src/main/java/com/sqbiq/javafxdemo/Calendar.java
@@ -1,13 +1,27 @@
 package com.sqbiq.javafxdemo;
 
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.time.Month;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class Calendar extends VBox {
     private CalendarGrid calendarGrid = new CalendarGrid();
+    private Text yearHeader = new Text();
+    private Text monthHeader = new Text();
+    private YearMonth date = YearMonth.now();
 
     public Calendar() {
+        yearHeader.setText(Integer.toString(date.getYear()));
+        getChildren().add(yearHeader);
+
+        monthHeader.setText(date.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()));
+        getChildren().add(monthHeader);
+
+        calendarGrid.setDate(date);
         getChildren().add(calendarGrid);
     }
 }
--- src/main/java/com/sqbiq/javafxdemo/CalendarGrid.java
+++ src/main/java/com/sqbiq/javafxdemo/CalendarGrid.java
@@ -4,27 +4,36 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
 import java.time.format.TextStyle;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
 
 public class CalendarGrid extends GridPane {
-    static final int ROWS_COUNT = 1 /*header*/ + 4 /*weeks*/;
+    static final int ROWS_COUNT = 1 /*header*/ + 5 /*weeks*/;
     static final int COLS_COUNT = 7 /*days of week*/;
+    private YearMonth date = YearMonth.now();
+    final WeekFields daysOfWeek = WeekFields.of(Locale.FRANCE);
 
     public CalendarGrid() {
         initDaysOfWeekRow();
         initGrid();
     }
 
-    static private Button getCell() {
-        Button cell = new Button("T");
+    public void setDate(YearMonth yearMonth) {
+        date = yearMonth;
+        initGrid();
+    }
+
+    static private Button getCell(String text) {
+        Button cell = new Button(text);
         cell.setMaxWidth(Double.MAX_VALUE);
         return cell;
     }
 
     private void initDaysOfWeekRow() {
-        final WeekFields daysOfWeek = WeekFields.of(Locale.FRANCE);
         DayOfWeek day = daysOfWeek.getFirstDayOfWeek();
         // for every week day
         for (int i = 0; i < 7; i++) {
@@ -35,10 +44,26 @@ public class CalendarGrid extends GridPane {
     }
 
     private void initGrid() {
-        for (int y = 1; y < ROWS_COUNT; y++) {
-            for (int x = 0; x < COLS_COUNT; x++) {
-                add(getCell(), x, y);
-            }
+        LocalDate monthly = LocalDate.of(date.getYear(), date.getMonth(), 1);
+        final DayOfWeek firstDayOfWeekInMonth = monthly.getDayOfWeek();
+        final DayOfWeek firstDayOfWeek = daysOfWeek.getFirstDayOfWeek();
+        final int daysInMonth = monthly.lengthOfMonth();
+
+        // fill cells gap
+        final int gap = Math.floorMod((firstDayOfWeekInMonth.getValue() - firstDayOfWeek.getValue()), 7);
+        int cell_i = 0;
+
+        for (; cell_i < gap; cell_i++) {
+            add(getCell(""), cell_i, 1);
+        }
+
+        // fill cells date
+        for (int cellCount = 0; cellCount < daysInMonth; cell_i++, cellCount++) {
+            int cellCol = cell_i % 7;
+            int cellRow = cell_i / 7;
+            String day = Integer.toString(monthly.getDayOfMonth());
+            add(getCell(day), cellCol, cellRow+1);
+            monthly = monthly.plusDays(1);
         }
     }
 }
