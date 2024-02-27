de3ea9ee392014e3bc507bf72e0820c9725dc556
Sebastian <sebasqbi@gmail.com>
2024-02-27 11:43:20 +0100

    Add YearField

---
+++ src/main/java/com/sqbiq/javafxdemo/elements/YearField.java
@@ -0,0 +1,176 @@
+package com.sqbiq.javafxdemo.elements;
+
+import javafx.scene.control.IndexRange;
+import javafx.scene.control.TextField;
+
+public class YearField extends TextField {
+    @Override
+    public String getText(int i, int i1) {
+        return super.getText(i, i1);
+    }
+
+    @Override
+    public void appendText(String s) {
+        super.appendText(s);
+    }
+
+    @Override
+    public void insertText(int i, String s) {
+        super.insertText(i, s);
+    }
+
+    @Override
+    public void deleteText(IndexRange indexRange) {
+        super.deleteText(indexRange);
+    }
+
+    @Override
+    public void deleteText(int i, int i1) {
+        super.deleteText(i, i1);
+    }
+
+    @Override
+    public void replaceText(IndexRange indexRange, String s) {
+        super.replaceText(indexRange, s);
+    }
+
+    @Override
+    public void replaceText(int i, int i1, String s) {
+        super.replaceText(i, i1, s);
+    }
+
+    @Override
+    public void cut() {
+        super.cut();
+    }
+
+    @Override
+    public void copy() {
+        super.copy();
+    }
+
+    @Override
+    public void paste() {
+        super.paste();
+    }
+
+    @Override
+    public void selectBackward() {
+        super.selectBackward();
+    }
+
+    @Override
+    public void selectForward() {
+        super.selectForward();
+    }
+
+    @Override
+    public void previousWord() {
+        super.previousWord();
+    }
+
+    @Override
+    public void nextWord() {
+        super.nextWord();
+    }
+
+    @Override
+    public void endOfNextWord() {
+        super.endOfNextWord();
+    }
+
+    @Override
+    public void selectPreviousWord() {
+        super.selectPreviousWord();
+    }
+
+    @Override
+    public void selectNextWord() {
+        super.selectNextWord();
+    }
+
+    @Override
+    public void selectEndOfNextWord() {
+        super.selectEndOfNextWord();
+    }
+
+    @Override
+    public void selectAll() {
+        super.selectAll();
+    }
+
+    @Override
+    public void home() {
+        super.home();
+    }
+
+    @Override
+    public void end() {
+        super.end();
+    }
+
+    @Override
+    public void selectHome() {
+        super.selectHome();
+    }
+
+    @Override
+    public void selectEnd() {
+        super.selectEnd();
+    }
+
+    @Override
+    public boolean deletePreviousChar() {
+        return super.deletePreviousChar();
+    }
+
+    @Override
+    public boolean deleteNextChar() {
+        return super.deleteNextChar();
+    }
+
+    @Override
+    public void forward() {
+        super.forward();
+    }
+
+    @Override
+    public void backward() {
+        super.backward();
+    }
+
+    @Override
+    public void positionCaret(int i) {
+        super.positionCaret(i);
+    }
+
+    @Override
+    public void selectPositionCaret(int i) {
+        super.selectPositionCaret(i);
+    }
+
+    @Override
+    public void selectRange(int i, int i1) {
+        super.selectRange(i, i1);
+    }
+
+    @Override
+    public void extendSelection(int i) {
+        super.extendSelection(i);
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+    }
+
+    @Override
+    public void deselect() {
+        super.deselect();
+    }
+
+    @Override
+    public void replaceSelection(String s) {
+        super.replaceSelection(s);
+    }
+}
