b6280993681793e5e2d745b726c958582a3a5b04
Sebastian <sebasqbi@gmail.com>
2024-02-29 21:30:25 +0100

    Remove calendar prototype

--- src/main/java/com/sqbiq/javafxdemo/CalendarApplication.java
+++ src/main/java/com/sqbiq/javafxdemo/CalendarApplication.java
@@ -18,37 +18,6 @@ import java.util.Locale;
 public class CalendarApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        final WeekFields daysOfWeek = WeekFields.of(Locale.FRANCE);
-//        final WeekFields daysOfWeek = WeekFields.of(Locale.getDefault());
-        DayOfWeek day = daysOfWeek.getFirstDayOfWeek();
-
-        for (int i = 0; i < 7 /*hopefully every locale has seven days in week*/; i++) {
-            System.out.printf("%s ", day.getDisplayName(TextStyle.SHORT, Locale.getDefault()));
-            day = day.plus(1);
-        }
-        System.out.println();
-
-        int selectedYear = 2024;
-        Month selectedMonth = Month.FEBRUARY;
-        LocalDate monthly = LocalDate.of(selectedYear, selectedMonth, 1);
-        final DayOfWeek firstDayOfWeekInMonth = monthly.getDayOfWeek();
-        final DayOfWeek firstDayOfWeek = daysOfWeek.getFirstDayOfWeek();
-        final DayOfWeek lastDayOfWeek = daysOfWeek.getFirstDayOfWeek().minus(1);
-        final int daysInMonth = monthly.lengthOfMonth();
-
-        // fill cells gap
-        final int gap = Math.floorMod((firstDayOfWeekInMonth.getValue() - firstDayOfWeek.getValue()), 7);
-        System.out.print("  X ".repeat(gap));
-
-        // fill cells with date
-        for (int i = 0; i < daysInMonth; i++) {
-            System.out.printf("%3d ", monthly.getDayOfMonth());
-            if (monthly.getDayOfWeek() == lastDayOfWeek)
-                System.out.println();
-            monthly = monthly.plusDays(1);
-        }
-
-
         Calendar root = new Calendar();
         Scene scene = new Scene(root);
 
